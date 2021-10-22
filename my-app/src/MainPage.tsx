d6b4d42bb11af2029f32562f04c553b0101b6def
tornal-dev <adam.warzecha00@gmail.com>
2021-10-23 00:11:06 +0200

    Added testing grid layout and cards

--- my-app/src/MainPage.tsx
+++ my-app/src/MainPage.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { MainPageBar } from "./components/MainPageBar";
+import BasicCard from "./components/BasicCard";
+import Grid from "@mui/material/Grid";
+import './MainPage.css';
 
 export class MainPage extends React.Component {
     public render() {
@@ -8,6 +11,22 @@ export class MainPage extends React.Component {
                 <MainPageBar
                     barText='Hi, Username'
                 />
+                <div className="GridPadding">
+                    <Grid container rowSpacing={8} columnSpacing={{ xs: 4, sm: 8, md: 12 }}>
+                        <Grid item xs={6}>
+                            <BasicCard/>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <BasicCard/>    
+                        </Grid>
+                        <Grid item xs={6}>
+                            <BasicCard/>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <BasicCard/>
+                        </Grid>
+                    </Grid>
+                </div>
             </React.Fragment>
         );
     }
