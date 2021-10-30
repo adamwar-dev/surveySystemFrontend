6672e601d96451c010ca2dd5ce95131ca350af24
AntoniLS <antoni.lis.dev@gmail.com>
2021-10-31 01:31:59 +0200

    LoginPage + pattern for styling

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { MainPage } from './MainPage';
+import {MainPage} from "./MainPage";
 
 class App extends React.Component {
 	public render () {
---
+++ my-app/src/styles/PalettePro.tsx
@@ -0,0 +1,12 @@
+
+export const palettePro = {
+    button: {
+        buttonPrimary: "#916BBF"
+    },
+    link: {
+        linkPrimary: "#713ebb"
+    },
+    frame:{
+        framePrimary: "#ac86f6"
+    }
+}
