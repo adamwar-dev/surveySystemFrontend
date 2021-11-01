3cc53c0eb7897f78e7bc00da231b3025085c9e6d
tornal-dev <adam.warzecha00@gmail.com>
2021-11-01 23:08:28 +0100

    moved MainPage to folder with Pages

--- my-app/src/pages/MainPage.tsx
+++ my-app/src/pages/MainPage.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { NavBar } from "./components/NavBar";
-import { Footer } from "./components/Footer";
+import { NavBar } from "../components/NavBar";
+import { Footer } from "../components/Footer";
 import './MainPage.css';
 export class MainPage extends React.Component {
 	public render() {
 		return (
 			<React.Fragment>
-				<NavBar/>
+				<NavBar barText="Hi, Pedale"/>
 				<Footer/>
 			</React.Fragment>
 		);
