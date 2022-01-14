ec0522840e838dc271fa2624709c5de430df3253
AntoniLS <antoni.lis.dev@gmail.com>
2022-01-15 00:50:07 +0100

    + paletteProSecondVersion

--- my-app/src/styles/PalettePro.tsx
+++ my-app/src/styles/PalettePro.tsx
@@ -10,3 +10,19 @@ export const palettePro = {
         framePrimary: "#ac86f6"
     }
 }
+
+export const paletteProSecondVersion = {
+    button: {
+        buttonPrimary: "#00A8CC"
+    },
+    link: {
+        linkPrimary: "#27496D"
+    },
+    frame:{
+        framePrimary: "#0C7B93"
+    },
+    additional:{
+        additionalPrimary: "#142850"
+    }
+}
+
